01370f113d28dfcafacde8cad7be6850ea0ce00b
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-11-09 23:11:36 -0300

    files updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './usuarios/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { PortadorModule } from './portadores/portador.module';
+import { AnelModule } from './aneis/anel.entity';
+import { UsuarioModule } from './portadores/portadores.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.Node_ENV || 'development'}`,
+    }),
+    UsersModule,
+    DatabaseModule,
+    PortadorModule,
+    AnelModule,
+    UsuarioModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
