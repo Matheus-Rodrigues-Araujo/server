611ac538ee056edec3c5b1b601411c819cd414db
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-11-10 23:21:28 -0300

    feat(aneis.module): added and exported TypeOrmModule

--- src/aneis/aneis.module.ts
+++ src/aneis/aneis.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+import { Anel } from './anel.entity';
+import { AneisController } from './aneis.controller';
+import { AneisService } from './aneis.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Anel])],
+  controllers: [AneisController],
+  providers: [AneisService],
+  exports: [TypeOrmModule],
+})
 export class AneisModule {}
