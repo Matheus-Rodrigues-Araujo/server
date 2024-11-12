20e7a0000933bba9f1e8a52006e2391baeb86247
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-11-11 23:54:17 -0300

    updateed

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { ConfigModule } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  })
   await app.listen(8000);
 }
 bootstrap();
